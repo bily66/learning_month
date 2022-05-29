ebcbeb2efe69295caebef0c724d974843b20327b
bily66 <97169724+bily66@users.noreply.github.com>
2022-05-29 22:27:01 +0800

    popup: process img

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,7 +31,7 @@ $('section.process .card-img').click(function() {
 
 $('.btn-close, .black.modal-black').click(function() {
     $('.modal-video iframe').attr('src', '');
-    $('.modal-process img').attr('src', '');
+    $('.modal-process img').attr('src', 'https://dummyimage.com/700x470/000/fff&text=+');
     $('.modal-process img').attr('alt', '');
     $('.modal-process h3').text('');
     $('.modal-process p').text('');
